214e2344fe5b25d7609c972b98c1578e3ac94660
Guilherme Amaral <94094527+guiopen@users.noreply.github.com>
2023-03-13 19:35:23 -0300

    oranges aligned

--- oranges.py
+++ oranges.py
@@ -4,12 +4,23 @@ draw = turtle.Turtle()
 
 #oranges start position
 draw.up()
+draw.speed(50)
 draw.back(430)
 
 #make a lot of oranges that dont touch each other
 for bola in range (40,140,10):
     draw.dot(bola,'orange')
-    draw.forward(10 + bola)
+    draw.forward(5 + bola)
+    draw.left(90)
+    draw.forward(5)
+    draw.rt(90)
+
+#show that oranges are aligned
+draw.color('black')
+draw.goto(-450,-20)
+draw.down()
+draw.width(3)
+draw.fd(900)
   
 
 

8f84082725b3eba5468cff9c3f980fc0fa40abde
Guilherme Amaral <94094527+guiopen@users.noreply.github.com>
2023-03-13 13:35:30 -0300

    Update oranges.py

--- oranges.py
+++ oranges.py
@@ -6,7 +6,7 @@ draw = turtle.Turtle()
 draw.up()
 draw.back(430)
 
-#make a lot of oranges thar dont touch each other
+#make a lot of oranges that dont touch each other
 for bola in range (40,140,10):
     draw.dot(bola,'orange')
     draw.forward(10 + bola)
